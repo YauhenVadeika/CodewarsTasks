4306c7e10dbfcee9ff58aff7aa577753eac951ae
Yauhen Vadeika <vadeikayauhen@gmail.com>
2022-07-28 00:26:53 +0300

    Add Tasks

---
+++ Task 1 Convert a Number to a String/Convert a Number to a String.py
@@ -0,0 +1,18 @@
+# my task solution
+def number_to_string(num):
+    # Return a string of the number here!
+    num = str(num)
+    return num
+
+
+# print(number_to_string(5))
+# print(type(number_to_string(5)))
+
+
+# codewars task best solution
+def number_to_string(num):
+    # Return a string of the number here!
+    return str(num)
+
+
+# print(number_to_string(5))
---
+++ Task 2 Even or Odd/Even or Odd.py
@@ -0,0 +1,18 @@
+# my task solution
+
+
+def even_or_odd(number):
+
+    number = "Even" if number % 2 == 0 else "Odd"
+    return number
+
+
+print(even_or_odd(1))
+
+
+# codewars task best solution
+def even_or_odd(number):
+    return 'Odd' if number % 2 else 'Even'
+
+
+print(even_or_odd(1))
