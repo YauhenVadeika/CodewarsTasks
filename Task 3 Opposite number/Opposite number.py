271e31802b867fa01a9b21283096b25f3e30ba37
Yauhen Vadeika <vadeikayauhen@gmail.com>
2022-07-29 16:32:56 +0300

    Add Task

---
+++ Task 3 Opposite number/Opposite number.py
@@ -0,0 +1,14 @@
+# my task solution
+def opposite(number):
+    return number * (-1)
+
+
+print(opposite(25.6))
+
+
+# codewars task best solution
+def opposite(number):
+    return -number
+
+
+print(opposite(25.5))
