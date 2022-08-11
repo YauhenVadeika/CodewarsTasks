4d1a4fae3f862dcf3e22c3b7ffa499dcfb9b9b8f
Yauhen Vadeika <vadeikayauhen@gmail.com>
2022-08-11 18:21:36 +0300

    Add Task

---
+++ Task 15 Vowel Count/Vowel Count.py
@@ -0,0 +1,23 @@
+# my task solution
+import re
+
+
+def get_count(sentence):
+    vowels = "aeiou"
+    return sum(item in vowels for item in sentence)
+
+
+print(get_count("abracadabra"))
+print(get_count(""))
+print(get_count("y"))
+print(get_count("aeiou"))
+
+
+# codewars task best solution
+def getCount(inputStr):
+    return sum(1 for let in inputStr if let in "aeiouAEIOU")
+
+
+# codewars task best solution
+def getCount(s):
+    return sum(c in 'aeiou' for c in s)
