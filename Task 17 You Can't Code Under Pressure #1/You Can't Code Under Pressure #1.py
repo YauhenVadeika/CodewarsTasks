84e0dfb2c5751160c4051cf26bab2570891f5082
Yauhen Vadeika <vadeikayauhen@gmail.com>
2022-08-11 19:47:19 +0300

    Add Task

---
+++ Task 17 You Can't Code Under Pressure #1/You Can't Code Under Pressure #1.py
@@ -0,0 +1,12 @@
+# my task solution
+def double_integer(i):
+    return 2 * i
+
+
+print(double_integer(-10))
+print(double_integer(100))
+
+
+# codewars task best solution
+def doubleInteger(i):
+    return i * 2
