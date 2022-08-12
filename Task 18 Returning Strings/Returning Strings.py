9440360b8d093e60af5ec9638db79f163d5d158f
Yauhen Vadeika <vadeikayauhen@gmail.com>
2022-08-12 13:14:18 +0300

    Add Task

---
+++ Task 18 Returning Strings/Returning Strings.py
@@ -0,0 +1,16 @@
+# my task solution
+def greet(name):
+    return f"Hello, {name} how are you doing today?"
+
+
+print(greet('Ryan'))
+
+
+# codewars task best solution
+def greet(name):
+    return f'Hello, {name} how are you doing today?'
+
+
+# codewars task best solution
+def greet(name):
+    return "Hello, {} how are you doing today?".format(name)
