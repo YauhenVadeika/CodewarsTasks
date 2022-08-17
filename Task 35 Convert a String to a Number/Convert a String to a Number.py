339e65650f429110f2ed43b04726de3d3063ca27
Yauhen Vadeika <vadeikayauhen@gmail.com>
2022-08-17 17:08:51 +0300

    Add Task

---
+++ Task 35 Convert a String to a Number/Convert a String to a Number.py
@@ -0,0 +1,12 @@
+# my task solution
+def string_to_number(s):
+    return int(s)
+
+
+print(string_to_number("1234"))
+print(string_to_number("-7"))
+
+
+# codewars task best solution
+def string_to_number(s):
+    return int(s)
