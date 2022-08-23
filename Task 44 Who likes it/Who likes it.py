bd82d036aa5b13288c4c1491d07243748ab755a4
Yauhen Vadeika <vadeikayauhen@gmail.com>
2022-08-23 19:50:46 +0300

    Add Task

---
+++ Task 44 Who likes it/Who likes it.py
@@ -0,0 +1,45 @@
+# my task solution
+def likes(names):
+
+    if not names:
+        return "no one likes this"
+    if len(names) == 1:
+        return f"{names[0]} likes this"
+    if len(names) == 2:
+        return f"{names[0]} and {names[1]} like this"
+    if len(names) == 3:
+        return f"{names[0]}, {names[1]} and {names[2]} like this"
+    else:
+        return f"{names[0]}, {names[1]} and {len(names) - 2} others like this"
+
+
+print(likes([]))
+print(likes(["Peter"]))
+print(likes(["Jacob", "Alex"]))
+print(likes(["Max", "John", "Mark"]))
+print(likes(["Alex", "Jacob", "Mark", "Max"]))
+
+
+# codewars task best solution
+def likes(names):
+    n = len(names)
+    return {
+        0: 'no one likes this',
+        1: '{} likes this',
+        2: '{} and {} like this',
+        3: '{}, {} and {} like this',
+        4: '{}, {} and {others} others like this'
+    }[min(4, n)].format(*names[:3], others=n - 2)
+
+
+# codewars task best solution
+def likes(names):
+    formats = {
+        0: "no one likes this",
+        1: "{} likes this",
+        2: "{} and {} like this",
+        3: "{}, {} and {} like this",
+        4: "{}, {} and {others} others like this"
+    }
+    n = len(names)
+    return formats[min(n, 4)].format(*names, others=n - 2)
