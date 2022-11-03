7d0808a5996a80822f27e764a73c8bd92235b5bb
Yauhen Vadeika <vadeikayauhen@gmail.com>
2022-11-03 21:33:19 +0300

    Add Task

---
+++ Task 115 Sentence Smash/Sentence Smash.py
@@ -0,0 +1,16 @@
+# my task solution
+def smash(words):
+    return ' '.join(words)
+
+
+print(smash(['hello', 'world', 'this', 'is', 'great']))
+
+
+# codewars task best solution
+def smash(words):
+    return " ".join(words)
+
+
+# codewars task best solution
+def smash(words):
+    return ' '.join(words).strip()
