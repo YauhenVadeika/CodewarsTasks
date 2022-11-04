f748cb723dd0b543dfee89347e8d45a2012a86f1
Yauhen Vadeika <vadeikayauhen@gmail.com>
2022-11-04 20:44:02 +0300

    Add Task

---
+++ Task 116 Convert a Boolean to a String/Sentence Smash.py
@@ -0,0 +1,24 @@
+# my task solution
+def boolean_to_string(b):
+    return str(b)
+
+
+print(boolean_to_string(True))
+print(boolean_to_string(False))
+
+
+# codewars task best solution
+def boolean_to_string(b):
+    return str(b)
+
+
+# codewars task best solution
+def boolean_to_string(b):
+    return 'True' if b else 'False'
+
+
+# codewars task best solution
+def boolean_to_string(b):
+    if b:
+        return "True"
+    return "False"
