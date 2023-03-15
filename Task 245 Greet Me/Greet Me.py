556ce474e568779bf40d0de10cfc41bd8d2ff220
Yauhen Vadeika <vadeikayauhen@gmail.com>
2023-03-15 23:46:26 +0300

    Add Task

---
+++ Task 245 Greet Me/Greet Me.py
@@ -0,0 +1,34 @@
+# my task solution
+def greet(name):
+
+    return f"Hello {name.capitalize()}!"
+
+
+print(greet('riley'))
+print(greet('molly'))
+print(greet('BILLY'))
+
+
+# codewars task best solution
+def greet(name):
+    return f'Hello {name.title()}!'
+
+
+# codewars task best solution
+def greet(name):
+    return f"Hello {name.capitalize()}!"
+
+
+# codewars task best solution
+def greet(name):
+    return "Hello " + name.capitalize() + "!"
+
+
+# codewars task best solution
+greet = lambda x: f"Hello {x.title()}!"
+
+
+# codewars task best solution
+def greet(n):
+    s = "Hello " + n.capitalize() + "!"
+    return s
