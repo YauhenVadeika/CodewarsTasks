a24dc688404585c514026b7d8bb830666da60ce7
Yauhen Vadeika <vadeikayauhen@gmail.com>
2023-03-29 18:27:29 +0300

    Add Task

---
+++ Task 259 Remove First and Last Character/Remove First and Last Character.py
@@ -0,0 +1,10 @@
+# my task solution
+def remove_char(s):
+    return s[1:-1]
+
+print(remove_char('eloquent'))
+
+# codewars task best solution
+"""You have not earned access to this kata's solutions
+Solutions are locked for kata ranked far above your rank. 
+Rank up or complete this kata to view the solutions. :)"""
