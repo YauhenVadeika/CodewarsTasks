4352c3ae7e0281aaf2421f2fb9288d886e153da5
Yauhen Vadeika <vadeikayauhen@gmail.com>
2023-03-30 17:05:04 +0300

    Add Task

---
+++ Task 260 Multiply/Multiply.py
@@ -0,0 +1,54 @@
+"""Dear "good people" from the Codewars Team (P.I.O.R.Y).
+I am very much concerned about your objectivity 
+in the decisions you have made.
+I keep moving on."""
+"""You have not earned access to this kata's solutions
+Solutions are locked for kata ranked far above your rank. 
+Rank up or complete this kata to view the solutions. :)"""
+
+# my task solution, my second life
+def multiply(a, b):
+    return a * b
+
+
+print(multiply(1, 1))
+
+
+# codewars task best solution
+def multiply(a, b):
+    return a * b
+
+
+# codewars task best solution
+def multiply(a, b):
+    if isinstance(a, (int, float, complex)):
+        if isinstance(b, (int, float, complex)):
+            return a * b
+
+
+# codewars task best solution
+def multiply(a, b):
+    return a * b  #added return statement
+
+
+# codewars task best solution
+#     __                       _               _
+#    / _|                     | |             (_)
+#   | |_ _ __ ___  ___    __ _| | _____  __    _  ___  _ __   ___  ___
+#   |  _| '__/ _ \/ _ \  / _` | |/ _ \ \/ /   | |/ _ \| '_ \ / _ \/ __|
+#   | | | | |  __/  __/ | (_| | |  __/>  <    | | (_) | | | |  __/\__ \
+#   |_| |_|  \___|\___|  \__,_|_|\___/_/\_\   | |\___/|_| |_|\___||___/
+#                                            _/ |
+#                                           |__/
+
+
+def multiply(a, b):
+    return a * b
+
+
+# codewars task best solution
+def multiply(a, b):
+    if a is not None and b is not None:
+        return a * b
+    else:
+        return None
